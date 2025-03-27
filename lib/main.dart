f1a6a863ea7ea52a020c8508c90f119a735d2d75
“Adham <adham.m.abdulah@gmail.com>
2025-03-28 00:50:38 +0200

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Weatherapp());
+}
+
+class Weatherapp extends StatelessWidget {
+  const Weatherapp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
