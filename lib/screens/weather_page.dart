1764dd13f12fb64e41e3650456081929395744cf
“Adham <adham.m.abdulah@gmail.com>
2025-03-28 15:18:21 +0200

    weather fetching logic

--- lib/screens/weather_page.dart
+++ lib/screens/weather_page.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_weather_app/service/weather_service.dart';
 
-class WeatherPage extends StatelessWidget {
+import '../model/weather_model.dart';
+
+class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  
+  // Instance of WeatherService to fetch weather data
+  final _weatherService = WeatherService();
+
+  // Variable to store the fetched weather data
+  WeatherModel? _weather;
+  //class to fetch the data from api
+  fetchweather() async {
+    // get the city name from method
+    final cityname = await _weatherService.getcity();
+    try {
+      final weather = await _weatherService.getweather(cityname);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print("error:$e");
+    }
+  }
+
+  @override
+  void initState() {
+    fetchweather();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      
 
     );
   }
--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -5,14 +5,13 @@ import 'package:minimal_weather_app/model/weather_model.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:http/http.dart' as http;
 
-class ClassName {
+class WeatherService {
   Future<WeatherModel> getweather(cityName) async {
     final response = await http.get(
       Uri.parse("$baseURI?q=$cityName&appid=$apiKey"),
     );
     if (response.statusCode == 200) {
       return WeatherModel.fromJson(jsonDecode(response.body));
-      // print(response.body);
     } else {
       throw Exception("fail");
     }
