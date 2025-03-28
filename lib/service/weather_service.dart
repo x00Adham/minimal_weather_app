7b261d8ec2392cb666986f052759f44a84b72a5f
“Adham <adham.m.abdulah@gmail.com>
2025-03-28 13:32:28 +0200

    Add method to retrieve city name based on GPS location

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -17,5 +17,25 @@ class ClassName {
       throw Exception("fail");
     }
   }
-
+    //get city name
+  Future<String> getcity() async {
+    //check for permission of the GPS
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      //get the permission
+      permission = await Geolocator.requestPermission();
+    }
+    //get position
+    Position position = await Geolocator.getCurrentPosition(
+      locationSettings: AndroidSettings(accuracy: LocationAccuracy.best),
+    );
+    //list of placemarks get the alt. and long. from geo position
+    List<Placemark> placemarks = await placemarkFromCoordinates(
+      position.altitude,
+      position.longitude,
+    );
+    //get name of city by placemark list
+    String? city = placemarks[0].locality;
+    return city ?? "";
+  }
 }
